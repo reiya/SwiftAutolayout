4e9d433a687390182d6c747e2edc494f855c93e6
matsuki-reiya <matsuki-reiya@Ebisu-PC-1475.local>
2016-06-22 20:37:47 +0900

    ファーストコミットプロジェクトを配置

---
+++ SwiftAutolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftAutolayout
+//
+//  Created by matsuki-reiya on 2016/06/22.
+//  Copyright © 2016年 matsuki-reiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
